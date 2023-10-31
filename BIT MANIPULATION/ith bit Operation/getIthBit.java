ddee1a7f955cce95cc02a31533b356a4caf40006
suvam <suvampravu@gmail.com>
2023-10-31 19:00:00 +0530

    update bit commit

--- BIT MANIPULATION/ith bit Operation/getIthBit.java
+++ BIT MANIPULATION/ith bit Operation/getIthBit.java
@@ -12,12 +12,12 @@ public class getIthBit {
     public static int setBit(int n, int i) {
         //Always set its value to 1
 
-//        int bitmask = 1 << i;
-//        return (n | bitmask);
+        int bitmask = 1 << i;
+        return (n | bitmask);
 
         //For set value to 0
-        int bitmask = ~(1 << i);
-        return (n & bitmask);
+//        int bitmask = ~(1 << i);
+//        return (n & bitmask);
 
     }
 
@@ -26,19 +26,26 @@ public class getIthBit {
         return n & bitmask;
     }
 
-    public static int update(int n,int i,int newBit){
-        if(newBit==0){
-            return clearBit(n,i);
-        }else {
-            return setBit(n,i);
-        }
+    public static int update(int n, int i, int newBit) {
+//        if(newBit==0){
+//            return clearBit(n,i);
+//        }else {
+//            return setBit(n,i);
+//        }
+
+        //Another Approach
+        n = clearBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+
+
     }
 
     public static void main(String[] args) {
-//        System.out.println(getBit(11, 2));
+        System.out.println(getBit(11, 2));
         System.out.println(setBit(10, 1));
-//        System.out.println(clearBit(10, 1));
-//        System.out.println(update(10,2,1));
+        System.out.println(clearBit(10, 1));
+        System.out.println(update(10, 0, 1));
 
     }
 }
