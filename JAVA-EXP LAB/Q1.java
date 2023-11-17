42ba09b3a7d39b9588a182540014393aca39bf8a
suvam <suvampravu@gmail.com>
2023-11-17 06:24:47 +0530

    commit my changes

--- JAVA-EXP LAB/Q1.java
+++ JAVA-EXP LAB/Q1.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
 public class Q1 {
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printDec(n - 1);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        String s = "tony";
-        System.out.println(s.toUpperCase());
+        int n = 10;
+        printDec(n);
     }
 }
---
+++ RECURSION/SumOfNumbers.java
@@ -0,0 +1,5 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        
+    }
+}
