1472f88d70cfea158dfd9edd1e73efc776d4004a
suvam <suvampravu@gmail.com>
2023-11-24 10:48:27 +0530

    power of num using recursion

---
+++ RECURSION/powerOfNum.java
@@ -0,0 +1,12 @@
+public class powerOfNum {
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2,5));
+    }
+}
