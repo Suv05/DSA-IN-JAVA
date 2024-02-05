37c87dd95898c54a957b337bb8493853260bd5e4
suvam <suvampravu@gmail.com>
2024-02-05 23:42:22 +0530

    commit factorial

---
+++ RECURSION/factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
+
