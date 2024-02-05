3a912fa1a7c1703567268d67cce40462c8a89679
suvam <suvampravu@gmail.com>
2024-02-05 23:42:49 +0530

    commit fibonaci number

--- RECURSION/fibonacciNumber.java
+++ RECURSION/fibonacciNumber.java
@@ -1,15 +1,16 @@
 public class fibonacciNumber {
-    public static int series(int n){
-        if(n==0){
+    public static int fibo(int n) {
+        if (n == 1) {
             return 0;
-        }else if(n==1){
+        } else if (n == 2) {
             return 1;
-        }else {
-            return series(n-1)+series(n-2);
+        } else {
+            return fibo(n-2) + fibo(n - 1);
         }
+
     }
+
     public static void main(String[] args) {
-        int nthDigit = 10;
-        System.out.println(series(nthDigit));
+        System.out.println(fibo(5));
     }
 }
