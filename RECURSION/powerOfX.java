2577b87d626522e165b95b7ce7782f6951397bf1
suvam <suvampravu@gmail.com>
2024-02-12 23:20:27 +0530

    commit optimized power of x to the power n

--- RECURSION/powerOfX.java
+++ RECURSION/powerOfX.java
@@ -1,16 +1,32 @@
 public class powerOfX {
-    public static int power(int x, int pow) {
+//    public static int power(int x, int pow) {
+//        if (pow == 0) {
+//            return 1;
+//        } else if (pow == 1) {
+//            return x;
+//        } else {
+//            return x * power(x, pow - 1);
+//        }
+//
+//    }
+
+
+    public static int optimizedPower(int x, int pow) {
         if (pow == 0) {
             return 1;
-        } else if (pow == 1) {
-            return x;
         } else {
-            return x * power(x, pow - 1);
-        }
+            int halfPower = optimizedPower(x, pow / 2);
+            int halfPowerSq = halfPower * halfPower;
+            //if odd
+            if (pow % 2 != 0) {
+                halfPowerSq = x * halfPowerSq;
+            }
 
+            return halfPowerSq;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(power(5, 4));
+        System.out.println(optimizedPower(5, 4));
     }
 }
