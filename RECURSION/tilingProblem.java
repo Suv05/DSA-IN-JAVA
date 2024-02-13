c916e70fc9c72e46d82a8cf0b92be3f08fb9321d
suvam <suvampravu@gmail.com>
2024-02-13 11:14:40 +0530

    commit tiling problem

---
+++ RECURSION/tilingProblem.java
@@ -0,0 +1,16 @@
+public class tilingProblem {
+    public static int noOfWays(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            int fnm1 = noOfWays(n - 1);
+            int fnm2 = noOfWays(n - 2);
+            return fnm2+fnm1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(noOfWays(2));
+
+    }
+}
