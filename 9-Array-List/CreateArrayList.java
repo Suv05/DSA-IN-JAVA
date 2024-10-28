c964f44790153678d25574ec38beb726da87a49d
suvam <suvampravu@gmail.com>
2024-10-28 23:53:25 +0530

    commit changes

---
+++ 9-Array-List/CreateArrayList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CreateArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println( list);
+    }
+}
