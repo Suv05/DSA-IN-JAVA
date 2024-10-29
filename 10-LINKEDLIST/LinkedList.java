02d3cdbe9e16e79ca4bcc9f9f42a6318a5354c7c
suvam <suvampravu@gmail.com>
2024-10-29 23:10:10 +0530

    commit changes

---
+++ 10-LINKEDLIST/LinkedList.java
@@ -0,0 +1,70 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    //methods to do all operations in a linked list
+
+    //addFirst()
+    public void addFirst(int data) {
+        //step-1 create a new node
+        Node newNode = new Node(data);
+
+        //if head is null means Linked List is not initialized then
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //step-2 stores head's address in next of newly created node
+        newNode.next = head;
+
+        //step-3 make the newly created node as new head
+        head = newNode;
+
+    }
+
+    //addLast
+    public void addLast(int data) {
+        //step-1 create a new node
+        Node newNode = new Node(data);
+
+        //if head is null means Linked List is not initialized then
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+
+        //step-2 now tails next should point the newly created node instead of null
+        tail.next = newNode;
+
+        //step-3 tail now move to newly created node
+        tail = newNode;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(4);
+        ll.addLast(5);
+
+
+        System.out.println(ll);
+    }
+}
