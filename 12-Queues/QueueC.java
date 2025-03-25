da46f20bff1680237d03c50c0455f256c24e0999
suvam <suvampravu@gmail.com>
2025-03-25 22:59:51 +0530

    Queues going on ✅

--- 12-Queues/QueueC.java
+++ 12-Queues/QueueC.java
@@ -1,5 +1,70 @@
-public class QueueC {
-    public static void main(String[] args) {
+//Queue implementation using linked list
 
+public class QueueC {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        public boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        public void add(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = tail = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            tail = newNode;
+
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+
+            }
+
+            int front = head.data;
+            //for single element
+            if (tail == head) {
+                tail = head = null;
+            } else {
+                head = head.next;
+            }
+            return front;
+        }
+
+        public int peek(){
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q=new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
 }
